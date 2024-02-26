de42a7f3c0002b632db912e2d84626a1643eaaa4
Abu Sadat Ansari <itssadatansari@gmail.com>
2024-02-26 22:21:22 +0530

    Add GameBoard and inside it placeShip

--- src/factories.js
+++ src/factories.js
@@ -21,4 +21,37 @@ function Ship(length) {
   };
 }
 
-export { Ship };
+function GameBoard() {
+  const Board = createBoard(10, 10);
+  let index = 0;
+  const shipList = [];
+
+  function placeShip(length, x, y) {
+    const ship = Ship(length);
+    for (let i = 0; i < length; i++) {
+      Board[x][y + i] = index;
+    }
+    index++;
+    shipList.push(ship);
+  }
+
+  function getBoard() {
+    return Board;
+  }
+
+  return { placeShip, getBoard };
+}
+
+function createBoard(rows, cols) {
+  const t1 = [];
+  for (let i = 0; i < rows; i++) {
+    const t2 = [];
+    for (let j = 0; j < cols; j++) {
+      t2.push(null);
+    }
+    t1.push(t2);
+  }
+  return t1;
+}
+
+export { Ship, GameBoard, createBoard };
--- tests/factories.test.js
+++ tests/factories.test.js
@@ -1,4 +1,4 @@
-import { Ship } from "../src/factories";
+import { GameBoard, Ship, createBoard } from "../src/factories";
 
 test("Ship Factory", () => {
   let destroyer = Ship(3);
@@ -10,4 +10,44 @@ test("Ship Factory", () => {
   destroyer.hit();
   expect(destroyer.getNoOfHits()).toBe(3);
   expect(destroyer.isSunk()).toBeTruthy();
+});
+
+test("Create Board", () => {
+  expect(createBoard(2, 2)).toEqual([
+    [null, null],
+    [null, null],
+  ]);
+});
+
+test("Place Ships", () => {
+  const gameBoard = GameBoard();
+  /* SHIP 1 TEST */
+  gameBoard.placeShip(3, 0, 1);
+  expect(gameBoard.getBoard()[0]).toEqual([
+    null,
+    0,
+    0,
+    0,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+
+  /* SHIP 2 TEST */
+  gameBoard.placeShip(5, 3, 1);
+  expect(gameBoard.getBoard()[3]).toEqual([
+    null,
+    1,
+    1,
+    1,
+    1,
+    1,
+    null,
+    null,
+    null,
+    null,
+  ]);
 });
