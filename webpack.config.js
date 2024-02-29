25724ca829f43316ecdcb4fc3a37c3f8877072cc
Abu Sadat Ansari <itssadatansari@gmail.com>
2024-02-29 07:26:26 +0530

    Setup more files and config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  entry: "./src/game.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
